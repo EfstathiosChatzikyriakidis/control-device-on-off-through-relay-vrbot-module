98ef301d4b3cc1d07dc16d4c778d1cac376122b7
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:27:06 +0300

    Change.

--- development/control_device_on_off_through_relay_vrbot_module/control_device_on_off_through_relay_vrbot_module.ino
+++ development/control_device_on_off_through_relay_vrbot_module/control_device_on_off_through_relay_vrbot_module.ino
@@ -1,7 +1,7 @@
 /*
  *  Control Device (On/Off) Through Relay & VRBot Module.
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
